b8c74fe91e14f9bac8a842adf5b6186e5cfce953
nerdynischal <nerdynischal@gmail.com>
2025-06-16 14:23:06 +0100

    Add js intearctivity on share button mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
 console.log("Linked and ready to go");
+
+const shareBtn = document.querySelector(".share-btn");
+const shareBtnClose = document.querySelector(".share-btn-close");
+const shareContainer = document.querySelector(".share-container");
+
+const card = document.querySelector(".card-container");
+
+shareBtn.addEventListener("click", () => {
+  shareContainer.style.display = "flex";
+});
+
+shareBtnClose.addEventListener("click", () => {
+  shareContainer.style.display = "none";
+});
