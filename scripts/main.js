3f6ef8869c92c5524ea152571f74b8e82a8cacb2
nerdynischal <nerdynischal@gmail.com>
2025-06-16 15:51:08 +0100

    Finish main functionality in both mobile and desktop

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,25 @@
 console.log("Linked and ready to go");
 
 const shareBtn = document.querySelector(".share-btn");
-const shareBtnClose = document.querySelector(".share-btn-close");
+const shareIcon = document.getElementById("share-icon");
+const iconContainer = document.querySelector(".icon-container");
 const shareContainer = document.querySelector(".share-container");
 
 const card = document.querySelector(".card-container");
 
 shareBtn.addEventListener("click", () => {
-  shareContainer.style.display = "flex";
-});
-
-shareBtnClose.addEventListener("click", () => {
-  shareContainer.style.display = "none";
+  if (shareContainer.style.display === "none") {
+    shareContainer.style.display = "flex";
+    shareIcon.style.fill = "white";
+    iconContainer.style.background = "var(--desaturated-dark-blue)";
+    //console.log(self.innerWidth);
+    if (self.innerWidth < 698) {
+      shareBtn.style.transform = "translateY(12px)";
+    }
+  } else {
+    shareIcon.style.fill = "#6E8098";
+    shareContainer.style.display = "none";
+    shareBtn.style.transform = "translateY(0px)";
+    iconContainer.style.background = "var(--light-grayish-blue)";
+  }
 });
